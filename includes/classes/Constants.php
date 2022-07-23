77f948b616f4ff9b7ed6207bc8142cc9a174ffbe
AnaSofOliveira <anasofiaoliveira.workmail@gmail.com>
2022-07-23 22:30:25 +0100

    Login form and errors

--- includes/classes/Constants.php
+++ includes/classes/Constants.php
@@ -2,6 +2,7 @@
 
     class Constants{
 
+        // Register Error Messages
         public static $passwordsDoNotMatch = "Your passwords don't match."; 
         public static $passwordNotAlphanumberic = "Your password can only contain numbers and letters.";
         public static $passwordCharacters = "Your username must be between 5 and 30 characters."; 
@@ -10,9 +11,11 @@
         public static $lastNameCharacters = "Your last name must be between 2 and 25 characters."; 
         public static $firstNameCharacters = "Your first name must be between 2 and 25 characters."; 
         public static $usernameCharacters = "Your username must be between 5 and 25 characters."; 
-
         public static $usernameTaken = "This username already exists.";
         public static $emailTaken = "This e-mail already exists.";
+
+        // Login Error Messages
+        public static $loginFailed = "Yout username or password was incorrect";
     
     }
 
