f0d3a577b78f446c6467689c911c0cc7504c2667
AnaSofOliveira <anasofiaoliveira.workmail@gmail.com>
2022-08-01 18:45:19 +0100

    Album info

---
+++ includes/classes/Artist.php
@@ -0,0 +1,24 @@
+<?php
+
+    class Artist{
+
+        private $con;
+        private $id; 
+
+        public function __construct($con, $id){
+            $this->con = $con;
+            $this->id = $id; 
+        }
+
+        public function getName(){
+            $artist_query = mysqli_query($this->con, "SELECT name FROM artists WHERE id='$this->id'"); 
+            $artist = mysqli_fetch_array($artist_query);
+
+            return $artist['name'];
+        }
+
+
+
+    }
+
+?>
