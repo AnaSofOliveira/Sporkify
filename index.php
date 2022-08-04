07c7475cd0647946e7f787935c48e8569108df7e
AnaSofOliveira <anasofiaoliveira.workmail@gmail.com>
2022-08-04 22:11:33 +0100

    Added browse page

--- index.php
+++ index.php
@@ -2,23 +2,7 @@
     include("includes/includedFiles.php");
 ?>
 
-<h1 class="pageHeadingBig"> You might also Like</h1>
+<script>openPage("browse.php")</script>
 
-<div class="gridViewContainer">
-
-    <?php 
-        $albumQuery = mysqli_query($con, "SELECT * FROM albums ORDER BY RAND() LIMIT 10");
-
-        while($row = mysqli_fetch_array($albumQuery)){
-            echo "<div class='gridViewItem'>
-            <span role='link' tabindex='0' onclick='openPage(\"album.php?id=".$row['id']."\")'>
-                        <img src='".$row['artworkPath']."'>
-                        <div class='gridViewInfo'>".$row['title']."</div>
-                    </span>
-                </div>";
-        }    
-    ?>
-    
-</div>
 
 
