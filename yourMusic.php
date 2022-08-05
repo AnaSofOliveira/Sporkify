eaa167de5e2dc5f239220ed87816bd545ae9cdfc
AnaSofOliveira <anasofiaoliveira.workmail@gmail.com>
2022-08-05 17:16:36 +0100

    Playlists

---
+++ yourMusic.php
@@ -0,0 +1,17 @@
+<?php 
+    include("includes/includedFiles.php");
+
+
+
+?>
+
+<div class="playlistsContainer">
+
+    <div class = gridViewContainer>
+        <h2>Playlists</h2>
+        <div class="buttonItems">
+            <button class="button green" onclick="createPlaylist()">New Playlist</button>
+        </div>
+    </div>
+
+</div>
