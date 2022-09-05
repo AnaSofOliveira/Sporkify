47dc1ad70dad10dda71ed1d6861bc82ee6272870
AnaSofOliveira <anasofiaoliveira.workmail@gmail.com>
2022-09-05 22:49:57 +0100

    Dark & Light Mode

--- settings.php
+++ settings.php
@@ -14,6 +14,6 @@
     <div class="buttonItems">
         <button class="button" onclick="openPage('updateDetails.php')">User Details</button>
         <button class="button" id="mode" onclick="changeThemeMode()">Light Mode</button>
-        <button class="button" onclick="logout()">Loggout</button>
+        <button class="button" onclick="logout()">Logout</button>
     </div>
 </div>
